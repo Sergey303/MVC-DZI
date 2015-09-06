6260a3c8c18b59efbecc4101782d94eccdb99db6
Sergey303 <s@s.s>
2015-09-06 16:04:04 +0600

    start

---
+++ WebDZCassetesDemo/DziController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Web.Mvc;
+using Fogid.DocumentStorage;
+
+namespace WebDZCassetesDemo
+{
+    [RoutePrefix("dzi")]
+    public class DziController : Controller
+    {
+        [Route("{cassetteName}/{dirName}/{fileName}_files/{level}/{x}_{y}.jpg")]
+        public ActionResult GetDZIJpeg(string cassetteName, string dirName, string fileName, string level, string x, string y)
+        {
+            return File(DStorage.GetFileFromSarc(cassetteName, dirName, fileName,
+                        string.Format("{0}_files/{1}/{2}_{3}.jpg", fileName, level, x, y)), "image/jpg");
+        }
+
+
+
+        [Route("{cassetteName}/{dirName}/{fileName}")]
+        public ActionResult DziXml(string cassetteName, string dirName, string fileName)
+        {
+            return File(DStorage.GetFileFromSarc(cassetteName, dirName, fileName, fileName+".xml"), "text/xml");
+        }
+
+        public string GetDZIXml(string id)
+        {
+            //XNamespace dzn = "http://schemas.microsoft.com/deepzoom/2009";
+            //return new XElement(dzn + "Image", new XAttribute(XNamespace.Xmlns + "dzn", dzn)  , new XAttribute("ServerFormat", "Default")).ToString();
+
+            return
+                "<?xml version='1.0' encoding='utf-8'?><Image TileSize='256' Overlap='1' Format='jpg' xmlns='http://schemas.microsoft.com/deepzoom/2009'>"
+                + GetDZISizeWidthHeight(id) + "</Image>";
+        }
+
+        private string GetDZISizeWidthHeight(string id)
+        {
+            //PaCell readImageCell = dzImgaseCells[id];//ReadImageCell(id,true);
+            //var lastLevel = readImageCell.Root.Elements().Last();
+            //var x = lastLevel.Elements().ToArray();
+            //int width = (x.Count() - 1) * 256;
+            //var y = x.Last().Elements().ToArray();
+            //int height = (y.Count() - 1) * 256;
+            //   Sarc.
+            //var img = Image.FromStream(new MemoryStream(((object[])y.Last().Get()).Cast<byte>().ToArray()));
+            ////   readImageCell.Close();
+            //width += img.Width;
+            //height += img.Height;
+
+            var sizeWidthHeight = String.Format("<Size Width='{0}' Height='{1}' />", 1000, 1000);
+            return sizeWidthHeight;
+        }
+
+        [Route("c/{dzcid}")]
+        public ActionResult DzcXml(string dzcid)
+        {
+          
+
+            //lock (StoreModel.Model)
+            //{
+            //    var absoluteUri = ControllerContext.HttpContext.Request.Url;
+            //    var currentUrl = absoluteUri.AbsoluteUri.Substring(0, absoluteUri.AbsoluteUri.Length - absoluteUri.AbsolutePath.Length);
+            //    return Content(StoreModel.Model.Store.GetDZC(dzcid.Split('_').Select(int.Parse), currentUrl), "text/xml");
+            //}
+            return new EmptyResult();
+
+        }
+        
+        [Route("dziview/{dzid:int}")]
+        public ActionResult ViewDeepZoomImage(int dzid)
+        {
+       return new EmptyResult();
+        }
+
+
+        //[Route("dzcview/{*dzcid}")]
+        //public ActionResult ViewDeepZoomCollection(string dzcid)
+        //{
+        //    return View("DeepZoomCollection", (object)dzcid);
+        //}
+        [Route("dzcview/{dzcid:int}")]
+        public ActionResult ViewDeepZoomCollection(int dzcid)
+        {
+            return new EmptyResult();
+
+        }
+    }
+}
---
+++ WebDZCassetesDemo/Global.asax.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Xml.Linq;
+using Fogid.DocumentStorage;
+
+namespace WebDZCassetesDemo
+{
+    public class Global : System.Web.HttpApplication
+    {
+        public static DStorage Storage;
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+              RouteTable.Routes.MapMvcAttributeRoutes();
+              //string path = "../../../TestData/";
+            string path = @"C:\cassettes\";
+              Storage = new DStorage();
+              Storage.Init(GetConfig(path));
+              DBAdapter adapter = new XmlDbAdapter(); //null;//new PBEAdapter();
+              Storage.InitAdapter(adapter);
+              Storage.LoadFromCassettesExpress();
+        }
+
+        public static XElement GetConfig(string path)
+        {
+            string config_text = @"<?xml version='1.0' encoding='utf-8' ?>
+<config>
+
+  <database dbname='polar20150825' connectionstring='polar:D:\home\FactographDatabases\polartest\'/>
+
+  <!--<LoadCassette regime='nodata'>" + path + "SampleOfCassette" + @"</LoadCassette>-->
+  <LoadCassette regime='nodata'>" + path + "newspaper" + @"</LoadCassette>
+  <!--LoadCassette write='yes'>D:\home\FactographProjects\Test20130213</LoadCassette-->
+</config>    
+";
+            return XElement.Parse(config_text);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
